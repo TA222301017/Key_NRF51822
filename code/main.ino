e492943abd5802ab4aa57760c88673c81341d60c
Laptop Taruna <muhamad.taruna@outlook.co.id>
2023-05-10 13:11:12 +0700

    update final main.ino

--- code/main.ino
+++ code/main.ino
@@ -1,50 +1,49 @@
 // importing libraries
+#include <AESLib.h>
 #include <BLEPeripheral.h>
 #include <SPI.h>
 
 // Defining PIN button and LED
-#define LED_PIN 10
-#define BUTTON_PIN 12
-
-// variabel for state management and message
-int stateButton = 0;
-int stateKeys = 0;
-
-// variabel for counting time
-unsigned long long startTime = 0;
+#define LED_PIN 3
+#define BUTTON_PIN 16
 
 // blePeripheral class
 BLEPeripheral blePeripheral = BLEPeripheral();
 
-// create service
+// create service and characteristic for auth
 BLEService communication = BLEService("19b10010-e8f2-537e-4f6c-d104768a1213");
-
-// create characteristic
-
-// requestMessage send request open door lock to central
-BLECharacteristic requestMessage =
-    BLECharacteristic("19b10010-e8f2-537e-4f6c-d104768a1214",
-                      BLERead | BLENotify, "                    ");
-
-// receiveMessage take encrypted message from central
-BLECharacteristic receiveMessage =
-    BLECharacteristic("19b10010-e8f2-537e-4f6c-d104768a1215",
-                      BLERead | BLEWrite | BLENotify, "                    ");
-
-// responseMessage send response for encryptedMessage to central
+BLECharacteristic receiveMessage = BLECharacteristic(
+    "19b10010-e8f2-537e-4f6c-d104768a1215",
+    BLERead | BLEWrite | BLENotify | BLEBroadcast, "                    ");
 BLECharacteristic responseMessage =
     BLECharacteristic("19b10010-e8f2-537e-4f6c-d104768a1217",
                       BLERead | BLENotify, "                    ");
 
-// debugging BLECharacteristic as serial print
-// stateKeyMessage notification keys state (1 if request open door'lock and 0
-// otherwise)
-BLEBoolCharacteristic stateKeyMessage = BLEBoolCharacteristic(
-    "19b10010-e8f2-537e-4f6c-d104768a1216", BLERead | BLENotify);
+// create service and characteristic for broadcast connectable
+BLEService broadService = BLEService("feaa");
+BLECharacteristic broadcastMessage =
+    BLECharacteristic("feab", BLERead | BLEBroadcast, 9);
 
-// lengthResponse give length of char*
-BLEIntCharacteristic lengthResponse = BLEIntCharacteristic(
-    "19b10010-e8f2-537e-4f6c-d104768a1218", BLERead | BLENotify);
+// Defining AES Class from library
+AESLib aesLib;
+
+// AES Encryption Key, secret key for encrypt and decrpyt
+byte aes_key[] = {0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31,
+                  0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31};
+
+// AES Initialization Vector, company's personal key for encrypt and decrpyt
+byte aes_iv[N_BLOCK] = {0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31,
+                        0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31};
+
+// variabel for state management and message
+int stateButton = 0;              // 0->unpushed, viseversa
+int stateKeys = 0;                // 0->unconnectable, viseversa
+unsigned long long startTime = 0; // Timeout counter
+
+const unsigned char device_template[4] = {0x53, 0x41, 0x43, 0x5F};
+const unsigned char device_id[4] = {0x30, 0x30, 0x30, 0x30};
+
+unsigned char serviceData[12];
 
 void setup() {
   // set LED pin to output mode, button pin to input mode
@@ -52,127 +51,160 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT);
 
   // set advertised local name and service UUID
-  // blePeripheral.setLocalName("NRF51822");
-  // blePeripheral.setDeviceName("NRF51822");
-  blePeripheral.setAppearance(1000);
+  blePeripheral.setLocalName((char *)device_id);
+  blePeripheral.setDeviceName((char *)device_id);
 
-  // set advertised UUID as indetifier keys
-  blePeripheral.setAdvertisedServiceUuid(
-      "19b10010-e8f2-537e-4f6c-d104768a1210");
-
-  // adding service and characteristic
+  // adding service and characteristic auth
   blePeripheral.addAttribute(communication);
-  blePeripheral.addAttribute(requestMessage);
   blePeripheral.addAttribute(receiveMessage);
-  blePeripheral.addAttribute(stateKeyMessage);
   blePeripheral.addAttribute(responseMessage);
-  blePeripheral.addAttribute(lengthResponse);
 
-  // begin initialization
+  // adding service and characteristic broadcastMessage connectable
+  blePeripheral.addAttribute(broadService);
+  blePeripheral.addAttribute(broadcastMessage);
+
+  for (int i = 0; i < 8; i++) {
+    if (i < 4) {
+      serviceData[i] = device_template[i];
+    } else {
+      serviceData[i] = device_id[i - 4];
+    }
+  }
+  serviceData[8] = 0x30; // Unconnectable
+  serviceData[9] = 0x00; // Reserved for future use, must be: 0x00
+  serviceData[10] = 0x00;
+  broadcastMessage.setValue(serviceData, 11);
+  blePeripheral.setAdvertisedServiceUuid("feaa");
+
+  // initialization BLE
   blePeripheral.begin();
+  broadcastMessage.broadcast();
+
+  // initialization AESLib
+  aesLib.gen_iv(aes_iv);
+  aesLib.set_paddingmode((paddingMode)0);
 }
 
-// function definition
-char *enkripsi(char *message);
-char *dekripsi(char *message);
+// General initialization vector
+byte enc_iv[N_BLOCK] = {0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31,
+                        0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31};
+byte enc_iv_to[N_BLOCK] = {0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31,
+                           0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31};
+byte enc_iv_from[N_BLOCK] = {0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31,
+                             0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31};
 
 void loop() {
-  // status connection
-  BLECentral central = blePeripheral.central();
+  // reset all variabel after booting
+  stateKeys = 0;
+  receiveMessage.setValue("                    ");
+  responseMessage.setValue("                    ");
 
-  // connected to central
-  if (central) {
-    // set LED ON for user notification connection ack
-    digitalWrite(LED_PIN, HIGH);
+  while (1) {
+    BLECentral central = blePeripheral.central();
 
-    // reset after connected for new processing
-    char *saveMessage = "                    ";
-    stateKeys = 0;
-    requestMessage.setValue("                    ");
-    stateKeyMessage.setValue(0);
-    responseMessage.setValue("                    ");
-    receiveMessage.setValue("                    ");
-    lengthResponse.setValue(0);
+    // check if button state changing, user make NRF51822 connectable
+    if (stateButton != digitalRead(BUTTON_PIN)) {
+      // take button value
+      stateButton = digitalRead(BUTTON_PIN);
+      // run if statebutton going HIGH
+      if (stateButton == 1) {
+        if (stateKeys == 0) {
+          // give statement to NRF its connectable
+          stateKeys = 1;
 
-    while (central.connected()) {
-      // check if button state changing, user request open door
-      if (stateButton != digitalRead(BUTTON_PIN)) {
-        // take button value
-        stateButton = digitalRead(BUTTON_PIN);
-
-        // run if statebutton going HIGH
-        if (stateButton == 1) {
-          if (stateKeys == 0) {
-            stateKeys = 1;
-            requestMessage.setValue("tolong buka pintu dong hehehe");
-            stateKeyMessage.setValue(1);
-            startTime = millis();
+          // give notification to PSOC connectable
+          for (int i = 0; i < 8; i++) {
+            if (i < 4) {
+              serviceData[i] = device_template[i];
+            } else {
+              serviceData[i] = device_id[i - 4];
+            }
           }
-        }
-      }
+          serviceData[8] = 0x31; // Connectable
+          serviceData[9] = 0x00; // Reserved for future use, must be: 0x00
+          serviceData[10] = 0x00;
+          broadcastMessage.setValue(serviceData, 11);
 
-      // processing if button is pushed
-      if (stateKeys == 1) {
-        // check for 3 second after button pushed
-        if ((millis() - startTime) > 3000) {
-          // reset all value
-          saveMessage = "                    ";
-          stateKeys = 0;
-          requestMessage.setValue("                    ");
-          stateKeyMessage.setValue(0);
-          responseMessage.setValue("                    ");
-          receiveMessage.setValue("                    ");
-          lengthResponse.setValue(0);
-        }
-
-        // convert const unsigned char * to char* message
-        unsigned char *_unsignedMessage =
-            const_cast<unsigned char *>(receiveMessage.value());
-        char *_saveMessage = reinterpret_cast<char *>(_unsignedMessage);
-        _saveMessage[receiveMessage.valueLength()] = '\0';
-
-        // processing after get newMessage from central
-        if (strcmp(_saveMessage, saveMessage) != 0) {
-
-          // copy new saveMessage from _saveMessage
-          saveMessage = (char *)malloc(receiveMessage.valueLength() + 1);
-          strcpy(saveMessage, _saveMessage);
-
-          // give response from central
-          char *encryptedMessage = enkripsi(saveMessage);
-          responseMessage.setValue(encryptedMessage);
-          lengthResponse.setValue(strlen(encryptedMessage));
+          // take time start connectable
+          startTime = millis();
         }
       }
     }
 
-    // set LED OFF for user notification connection terminated
-    digitalWrite(LED_PIN, LOW);
+    // stateKeys -> 0 unconnectable, stateKeys -> connectable
+    if (stateKeys == 1) {
+
+      // connected to central
+      if (central) {
+        // set LED ON for user notification connection ack
+        digitalWrite(LED_PIN, HIGH);
+
+        // reset to init value after connected
+        char resultMessage[16] = {0};
+        receiveMessage.setValue("                    ");
+        responseMessage.setValue("                    ");
+
+        // auth
+        while (central.connected()) {
+
+          // processing after get newMessage from central
+          if (receiveMessage.written()) {
+
+            // take chippertext from central
+            unsigned char *_chippertext =
+                const_cast<unsigned char *>(receiveMessage.value());
+            char *chippertext = reinterpret_cast<char *>(_chippertext);
+            chippertext[receiveMessage.valueLength()] = '\0';
+
+            // decryption message
+            memcpy(enc_iv, enc_iv_from, sizeof(enc_iv_from));
+            aesLib.decrypt((unsigned char *)chippertext, strlen(chippertext),
+                           (byte *)resultMessage, aes_key, sizeof(aes_key),
+                           enc_iv);
+            responseMessage.setValue((char *)resultMessage);
+          }
+        }
+
+        // event central disconnect
+        digitalWrite(LED_PIN, LOW); // user notification connection terminatte
+        stateKeys = 0;              // state keys for unconnectable
+
+        // send notification to PSOC unconnectable
+        for (int i = 0; i < 8; i++) {
+          if (i < 4) {
+            serviceData[i] = device_template[i];
+          } else {
+            serviceData[i] = device_id[i - 4];
+          }
+        }
+        serviceData[8] = 0x30; // Unconnectable
+        serviceData[9] = 0x00; // Reserved for future use, must be: 0x00
+        serviceData[10] = 0x00;
+        broadcastMessage.setValue(serviceData, 11);
+      }
+
+      // check timeout time
+      if ((millis() - startTime) > 3000) {
+        // make NRF unconnectable
+        stateKeys = 0;
+
+        // send notification to PSOC unconnectable
+        for (int i = 0; i < 8; i++) {
+          if (i < 4) {
+            serviceData[i] = device_template[i];
+          } else {
+            serviceData[i] = device_id[i - 4];
+          }
+        }
+        serviceData[8] = 0x30; // Unconnectable
+        serviceData[9] = 0x00; // Reserved for future use, must be: 0x00
+        serviceData[10] = 0x00;
+        broadcastMessage.setValue(serviceData, 11);
+      }
+
+    } else {
+      //     peripheral always disconnect until user push button
+      central.disconnect();
+    }
   }
-}
-
-char *enkripsi(char *message) {
-  // function menambahkan nilai 1 ASCII ke setiap char
-
-  char *encryptedMessage;
-  // take length string
-  int lenghtChar = strlen(message);
-
-  // copy string first
-  // used for saveMessage didnt change
-  encryptedMessage = (char *)malloc(lenghtChar + 1);
-  strcpy(encryptedMessage, message);
-
-  // processing +1 in ascii
-  for (int i = 0; i < lenghtChar; i++) {
-    encryptedMessage[i] = message[i] + 1;
-  }
-  encryptedMessage[lenghtChar] = '\0';
-
-  return encryptedMessage;
-}
-
-char *dekripsi(char *message) {
-  char *decryptedMessage;
-  // function here
 }
